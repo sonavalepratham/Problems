aeb52b2c2c9ed3122639fddcb436a61a64de3ae4
sonavalepratham <sonavaleps@gmail.com>
2022-10-19 23:45:26 +0530

    cses_increasing_array.cpp cses_permutations.cpp

---
+++ cses_permutations.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 	int n; cin>>n;
+ 	if(n==1){
+ 		cout<<1; return 0;
+ 	}
+ 	if(n<4){
+ 		cout<<"NO SOLUTION";
+ 		return 0;
+ 	}   
+ 	for(int i=2; i<=n; i+=2) cout<<i<<" ";
+ 	for(int i=1; i<=n; i+=2) cout<<i<<" ";
+    return 0;
+}
